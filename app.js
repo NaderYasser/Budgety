1ac3f7472b5ec04dd5a924abf25327b81a2d6477
Nader Yasser <naderdd7@gmail.com>
2018-02-23 15:51:02 +0200

    Budget Controller

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 
 // BUDGET CONTROLLER
 var budgetController = (function () {
-
-
+    // object constructor
+    var Expense = function (id, description, value) {
+      this.id = id;
+      this.description = description;
+      this.value = value;
+    }
+    var Income = function (id, description, value) {
+      this.id = id;
+      this.description = description;
+      this.value = value;
+    }
+    var Data = {
+      allItems : {
+        exp : [],
+        inc : []
+      },
+      totals: {
+        exp:0,
+        inc : 0
+      }
+    }
 
 
 
